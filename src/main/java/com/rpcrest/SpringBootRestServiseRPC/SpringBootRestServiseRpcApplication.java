0512ebb42ad3437cca6186756b924e88685c9bc1
Фёдор Ситников <mr.prizrek@mail.ru>
2023-12-13 11:38:22 +0300

    Init comment

---
+++ src/main/java/com/rpcrest/SpringBootRestServiseRPC/SpringBootRestServiseRpcApplication.java
@@ -0,0 +1,13 @@
+package com.rpcrest.SpringBootRestServiseRPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestServiseRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestServiseRpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpcrest/SpringBootRestServiseRPC/SpringBootRestServiseRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpcrest.SpringBootRestServiseRPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestServiseRpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
